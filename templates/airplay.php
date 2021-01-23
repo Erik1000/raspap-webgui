281020ff03eeaa946780e081a636bd1e778b8f95
Erik1000 <erik1000@protonmail.com>
2021-01-23 13:23:33 +0100

    blablabla

--- templates/airplay.php
+++ templates/airplay.php
@@ -8,14 +8,13 @@
             </div>
             <div class="col">
               <button class="btn btn-light btn-icon-split btn-sm service-status float-right">
-                <span class="icon text-gray-600"><i class="fas fa-circle service-status-green"></i></span>
+                <span class="icon text-gray-600"><i class="fas fa-circle service-status-down"></i></span>
                 <span class="text service-status">airplay server up/down add here</span>
               </button>
             </div>
           </div><!-- /.row -->
         </div><!-- /.card-header -->
         <div class="card-body">
-        <?php $status->showMessages(); ?>
           <form role="form" action="?page=openvpn_conf" enctype="multipart/form-data" method="POST">
             <?php echo CSRFTokenFieldTag() ?>
             <!-- Tab panes -->
