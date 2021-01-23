f76c52e0da7b0383c8dc3ecaac760cb7ec2d8d91
Erik1000 <erik1000@protonmail.com>
2021-01-23 13:40:54 +0100

    change button label

--- templates/airplay.php
+++ templates/airplay.php
@@ -19,9 +19,7 @@
             <?php echo CSRFTokenFieldTag() ?>
             <!-- Tab panes -->
             <div class="tab-content">
-                  <input type="submit" class="btn btn-success" name="startAirplayServer" value="Start Airplay server" />
-                  <input type="submit" class="btn btn-success" name="stopAirplayServer" value="Start OpenVPN" />
-                  <label for="audiotype" class="form-control-label col-md-3 col-8">Send audio via</label>
+                  <label for="audiotype">Send audio via</label>
                   <div class="o-switch btn-group" id="audiotype" data-toggle="buttons" role="group">
                     <label class="btn btn-secondary active">
                       <input type="radio" name="options" id="option1" autocomplete="off" checked>HDMI
@@ -32,7 +30,9 @@
                     <label class="btn btn-secondary">
                       <input type="radio" name="options" id="option3" autocomplete="off">None (disabled)
                     </label>
-                </div>
+                  </div>
+                  <input type="submit" class="btn btn-success" name="startAirplayServer" value="Start Airplay server" />
+                  <input type="submit" class="btn btn-danger" name="stopAirplayServer" value="Stop Airplay server" />
               </form>
             </div>
         </div><!-- /.card-body -->
