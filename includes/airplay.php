62ef69ed29e1c63ef03512eea6c5300fbbb69d64
Erik1000 <erik1000@protonmail.com>
2021-01-24 12:47:37 +0100

    update up/down status

--- includes/airplay.php
+++ includes/airplay.php
@@ -12,19 +12,18 @@ function DisplayAirplay()
 {
     $status = new StatusMessages();
     exec('pidof rpiplay', $airplaypid, $killable);
-
     if (!RASPI_MONITOR_ENABLED) {
         if (isset($_POST["stopAirplayServer"])) {
             if ($killable == 0) {
-                exec("/usr/local/bin/stop_rpiplay.sh", $return, $code);
-                $status->addMessage("Stopped the airplay server with pid $airplaypid (returned $code)", 'success');
+                exec("sudo /usr/local/bin/stop_rpiplay.sh", $return, $code);
+                $status->addMessage("Stopped the airplay server ($code).", 'success');
                 foreach ($return as $line) {
                     $status->addMessage($line, 'info');
                 }
             }
         } elseif (isset($_POST["killAirplayServer"])) {
             exec("sudo /usr/local/bin/kill_rpiplay.sh", $return, $s);
-            $status->addMessage("Killed the airplay server with pid $airplaypid (returned $code)", 'danger');
+            $status->addMessage("Killed the airplay server ($code).", 'success');
             foreach ($return as $line) {
                 $status->addMessage($line, 'info');
             }
@@ -36,7 +35,7 @@ function DisplayAirplay()
             }
         }
     }
-
+    exec('pidof rpiplay', $airplaypid, $killable);
     $serviceStatus = $killable == 0 ? "up" : "down";
     echo renderTemplate(
         "airplay", compact(
