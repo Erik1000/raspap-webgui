8a07cb3019c0226face17e2f2cb47e12bf5e68b1
Erik1000 <erik1000@protonmail.com>
2021-01-24 12:51:33 +0100

    update up/down status

--- includes/airplay.php
+++ includes/airplay.php
@@ -11,10 +11,10 @@ require_once 'includes/config.php';
 function DisplayAirplay()
 {
     $status = new StatusMessages();
-    exec('pidof rpiplay', $airplaypid, $killable);
+    exec('pidof rpiplay', $airplaypid, $running);
     if (!RASPI_MONITOR_ENABLED) {
         if (isset($_POST["stopAirplayServer"])) {
-            if ($killable == 0) {
+            if ($running == 0) {
                 exec("sudo /usr/local/bin/stop_rpiplay.sh", $return, $code);
                 $status->addMessage("Stopped the airplay server ($code).", 'success');
                 foreach ($return as $line) {
@@ -22,20 +22,25 @@ function DisplayAirplay()
                 }
             }
         } elseif (isset($_POST["killAirplayServer"])) {
-            exec("sudo /usr/local/bin/kill_rpiplay.sh", $return, $s);
-            $status->addMessage("Killed the airplay server ($code).", 'success');
-            foreach ($return as $line) {
-                $status->addMessage($line, 'info');
+            if ($running == 0) {
+                exec("sudo /usr/local/bin/kill_rpiplay.sh", $return, $s);
+                $status->addMessage("Killed the airplay server ($code).", 'success');
+                foreach ($return as $line) {
+                    $status->addMessage($line, 'info');
+                }
             }
         } elseif (isset($_POST["startAirplayServer"])) {
-            exec("sudo /usr/local/bin/rpiplay -b on > /dev/null 2>/dev/null &", $return, $code);
-            $status->addMessage("Started Airplay server. (returned $code)", "success");
-            foreach ($return as $line) {
-                $status->addMessage($line, 'info');
+            if ($running != 0) {
+                exec("sudo /usr/local/bin/rpiplay -b on > /dev/null 2>/dev/null &", $return, $code);
+                $status->addMessage("Started Airplay server. (returned $code)", "success");
+                foreach ($return as $line) {
+                    $status->addMessage($line, 'info');
+                }
             }
         }
     }
     exec('pidof rpiplay', $airplaypid, $killable);
+    
     $serviceStatus = $killable == 0 ? "up" : "down";
     echo renderTemplate(
         "airplay", compact(
