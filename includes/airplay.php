d22614e6eee801cad156f13866e1e723294dca45
Erik1000 <erik1000@protonmail.com>
2021-01-24 13:31:48 +0100

    just trying

--- includes/airplay.php
+++ includes/airplay.php
@@ -12,35 +12,33 @@ function DisplayAirplay()
 {
     $status = new StatusMessages();
     exec('pidof rpiplay', $airplaypid, $killable);
-    if ($_SERVER["REQUEST_METHOD" == "POST"]) {
-        if (!RASPI_MONITOR_ENABLED) {
-            if (isset($_POST["stopAirplayServer"])) {
-                if ($killable == 0) {
-                    exec("sudo /usr/local/bin/stop_rpiplay.sh", $return, $code);
-                    $status->addMessage("Stopped the airplay server ($code).", 'success');
-                    foreach ($return as $line) {
-                        $status->addMessage($line, 'info');
-                    }
+    if (!RASPI_MONITOR_ENABLED) {
+        if (isset($_POST["stopAirplayServer"])) {
+            if ($killable == 0) {
+                exec("sudo /usr/local/bin/stop_rpiplay.sh", $return, $code);
+                $status->addMessage("Stopped the airplay server ($code).", 'success');
+                foreach ($return as $line) {
+                    $status->addMessage($line, 'info');
+                }
+            }
+            $killable = 1;
+        } elseif (isset($_POST["killAirplayServer"])) {
+            if ($killable == 0) {
+                exec("sudo /usr/local/bin/kill_rpiplay.sh", $return, $code);
+                $status->addMessage("Killed the airplay server ($code).", 'success');
+                foreach ($return as $line) {
+                    $status->addMessage($line, 'info');
                 }
                 $killable = 1;
-            } elseif (isset($_POST["killAirplayServer"])) {
-                if ($killable == 0) {
-                    exec("sudo /usr/local/bin/kill_rpiplay.sh", $return, $code);
-                    $status->addMessage("Killed the airplay server ($code).", 'success');
-                    foreach ($return as $line) {
-                        $status->addMessage($line, 'info');
-                    }
-                    $killable = 1;
+            }
+        } elseif (isset($_POST["startAirplayServer"])) {
+            if ($killable != 0) {
+                exec("sudo /usr/local/bin/rpiplay -b on > /dev/null 2>/dev/null &", $return, $code);
+                $status->addMessage("Started Airplay server. (returned $code)", "success");
+                foreach ($return as $line) {
+                    $status->addMessage($line, 'info');
                 }
-            } elseif (isset($_POST["startAirplayServer"])) {
-                if ($killable != 0) {
-                    exec("sudo /usr/local/bin/rpiplay -b on > /dev/null 2>/dev/null &", $return, $code);
-                    $status->addMessage("Started Airplay server. (returned $code)", "success");
-                    foreach ($return as $line) {
-                        $status->addMessage($line, 'info');
-                    }
-                    $killable = 0;
-                }
+                $killable = 0;
             }
         }
     }
