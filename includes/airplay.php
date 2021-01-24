86b61f5f7165505f13a225eb5b57e7ceeb7f2766
Erik1000 <erik1000@protonmail.com>
2021-01-24 12:43:10 +0100

    should ru now. remember me to write a readme what to do to make it run

--- includes/airplay.php
+++ includes/airplay.php
@@ -16,20 +16,20 @@ function DisplayAirplay()
     if (!RASPI_MONITOR_ENABLED) {
         if (isset($_POST["stopAirplayServer"])) {
             if ($killable == 0) {
-                exec("sudo /bin/kill $airplaypid", $return, $code);
+                exec("/usr/local/bin/stop_rpiplay.sh", $return, $code);
                 $status->addMessage("Stopped the airplay server with pid $airplaypid (returned $code)", 'success');
                 foreach ($return as $line) {
                     $status->addMessage($line, 'info');
                 }
             }
         } elseif (isset($_POST["killAirplayServer"])) {
-            exec("sudo /bin/kill -SIGKILL $airplaypid", $return, $s);
+            exec("sudo /usr/local/bin/kill_rpiplay.sh", $return, $s);
             $status->addMessage("Killed the airplay server with pid $airplaypid (returned $code)", 'danger');
             foreach ($return as $line) {
                 $status->addMessage($line, 'info');
             }
         } elseif (isset($_POST["startAirplayServer"])) {
-            exec("/usr/local/bin/rpiplay -b on > /dev/null 2>/dev/null &", $return, $code);
+            exec("sudo /usr/local/bin/rpiplay -b on > /dev/null 2>/dev/null &", $return, $code);
             $status->addMessage("Started Airplay server. (returned $code)", "success");
             foreach ($return as $line) {
                 $status->addMessage($line, 'info');
